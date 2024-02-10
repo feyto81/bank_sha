357714844186d973aee869ba4f9de7d838108276
feyto81 <feyto81@gmail.com>
2024-02-10 18:03:13 +0700

    feat(logo in sign in page called)

--- lib/ui/pages/sign_in_page.dart
+++ lib/ui/pages/sign_in_page.dart
@@ -1,3 +1,4 @@
+import 'package:bank_sha_learn/shared/theme.dart';
 import 'package:flutter/material.dart';
 
 class SignInPage extends StatelessWidget {
@@ -5,6 +6,30 @@ class SignInPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: ligthBackgroundColor,
+      body: ListView(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 24,
+        ),
+        children: [
+          Container(
+            width: 155,
+            height: 50,
+            margin: const EdgeInsets.only(
+              top: 100,
+              bottom: 100,
+            ),
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(
+                  'assets/img_logo_light.png',
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
