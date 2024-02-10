371008804f2ce98f751606275d7c053759c0d923
feyto81 <feyto81@gmail.com>
2024-02-10 15:46:13 +0700

    feat(splash flase screen)

--- lib/ui/pages/splash_flash.dart
+++ lib/ui/pages/splash_flash.dart
@@ -1,3 +1,4 @@
+import 'package:bank_sha_learn/shared/theme.dart';
 import 'package:flutter/material.dart';
 
 class SplashPage extends StatelessWidget {
@@ -5,6 +6,16 @@ class SplashPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: darkBackgroundColor,
+      body: Center(
+          child: Container(
+        width: 155,
+        height: 50,
+        decoration: BoxDecoration(
+            image:
+                DecorationImage(image: AssetImage('assets/img_logo_dark.png'))),
+      )),
+    );
   }
 }
