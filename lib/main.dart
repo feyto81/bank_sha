8566423740c69aac30acb0c767f5fed75637bacd
feyto81 <feyto81@gmail.com>
2024-02-10 13:55:04 +0700

    feat(text style)

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bank_sha_learn/shared/theme.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -78,7 +79,12 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
           // Center is a layout widget. It takes a single child and positions it
           // in the middle of the parent.
-          child: Image.asset('assets/img_logo_dark.png')),
+          child: Text(
+        'Hello, Semua!',
+        style: blueTextStyle.copyWith(
+          fontSize: 30,
+        ),
+      )),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
