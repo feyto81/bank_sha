d4c711cecb864c715db801b635d8f176d1e1bb51
feyto81 <feyto81@gmail.com>
2024-02-12 18:39:43 +0700

    feat(Adding widget textwidget in button widget

--- lib/ui/widgets/button.dart
+++ lib/ui/widgets/button.dart
@@ -41,3 +41,38 @@ class CustomFilledButton extends StatelessWidget {
     );
   }
 }
+
+class CustomTextWidget extends StatelessWidget {
+  final String title;
+  final double width;
+  final double height;
+  final VoidCallback? onPressed;
+
+  const CustomTextWidget({
+    Key? key,
+    required this.title,
+    this.width = double.infinity,
+    this.height = 24,
+    this.onPressed,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: width,
+      height: height,
+      child: TextButton(
+        onPressed: () {},
+        style: TextButton.styleFrom(
+          padding: EdgeInsets.zero,
+        ),
+        child: Text(
+          title,
+          style: greyTextStyle.copyWith(
+            fontSize: 16,
+          ),
+        ),
+      ),
+    );
+  }
+}
