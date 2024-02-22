d247ce1f4e2310c6face683c7c1d01942c9cabef
feyto81 <feyto81@gmail.com>
2024-02-22 20:08:46 +0700

    feat(Sign Up Upload Profile Page):initial commit

---
+++ lib/ui/pages/sign_up_upload_profile_page.dart
@@ -0,0 +1,94 @@
+import 'package:bank_sha_learn/shared/theme.dart';
+import 'package:bank_sha_learn/ui/widgets/button.dart';
+import 'package:bank_sha_learn/ui/widgets/forms.dart';
+import 'package:flutter/material.dart';
+
+class SignUpUploadProfilePage extends StatelessWidget {
+  const SignUpUploadProfilePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: ligthBackgroundColor,
+      body: ListView(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 24,
+        ),
+        children: [
+          Container(
+            width: 155,
+            height: 50,
+            margin: const EdgeInsets.only(
+              top: 100,
+              bottom: 100,
+            ),
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(
+                  'assets/img_logo_light.png',
+                ),
+              ),
+            ),
+          ),
+          Text(
+            'Join Us to Unlock\nYour Growth',
+            style: blackTextStyle.copyWith(
+              fontSize: 20,
+              fontWeight: semiBold,
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Container(
+            padding: const EdgeInsets.all(22),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              color: whiteColor,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // Full name
+                const CustomFormField(
+                  title: 'Full Name',
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                // NOTE: EMAIL INPUT
+                const CustomFormField(
+                  title: 'Email Address',
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                // NOTE: PASSWORD INPUT
+                const CustomFormField(
+                  title: 'Password',
+                  obscureText: true,
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                CustomFilledButton(
+                  title: 'Continue',
+                  onPressed: () {},
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 50,
+          ),
+          CustomTextButton(
+            title: 'Sign In',
+            onPressed: () {
+              Navigator.pushNamed(context, '/sign-in');
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
